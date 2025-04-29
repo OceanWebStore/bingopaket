da07101df41e80e44193ad441a2fa856938f8ec8
OceanWebStore <oceanwebstore53@gmail.com>
2025-04-29 13:05:00 +0300

    Update restopanel.php

--- restoran/restopanel.php
+++ restoran/restopanel.php
@@ -258,6 +258,8 @@
     <!-- Vendor Javascript -->
     <script src="assets/js/vendor.min.js"></script>
     <script src="assets/js/app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
 
     <!-- Form Submit Handler -->
     <script>
